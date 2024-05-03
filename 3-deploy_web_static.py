07d5a71375a2e1f1bda1dc65c8eee4983afed0bd
MoodyEG <moodyeg2011@yahoo.com>
2024-05-03 14:50:26 -0700

    done fast

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -21,7 +21,7 @@ def deploy():
 
 def do_pack():
     """generates a .tgz archive from the contents of the web_static"""
-    timenow = strftime("%Y%M%d%H%M%S")
+    timenow = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
         filename = "versions/web_static_{}.tgz".format(timenow)
