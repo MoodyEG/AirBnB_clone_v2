83b9e43fcd5fd0f5ae7d810a0596a851b3930360
MoodyEG <moodyeg2011@yahoo.com>
2024-05-03 14:20:47 -0700

    done fast

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,5 +11,5 @@ def do_pack():
         filename = "versions/web_static_{}.tgz".format(timenow)
         local("tar -cvzf {} web_static/".format(filename))
         return filename
-    except:
+    except Exception:
         return None
---
+++ 3-deploy_web_static.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" Main
+Usage:
+fab -f 2-do_deploy_web_static.py
+do_deploy:archive_path=versions/web_static_20170315003959.tgz
+-i my_ssh_private_key -u ubuntu
+"""
+from fabric.api import *
+import os.path
+from time import strftime
+
+
+env.hosts = ['52.23.178.135', '100.25.47.15']
+env.user = "ubuntu"
+
+
+def deploy():
+    """ does both functions below """
+    try:
+        archive_path = do_pack()
+        if archive_path is None:
+            return False
+    except Exception:
+        return False
+    return do_deploy(archive_path)
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
+    timenow = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        filename = "versions/web_static_{}.tgz".format(timenow)
+        local("tar -cvzf {} web_static/".format(filename))
+        return filename
+    except Exception:
+        return None
+
+
+def do_deploy(archive_path):
+    """ distributes an archive to your web servers """
+    if os.path.isfile(archive_path) is False:
+        return False
+    try:
+        filename = archive_path.split("/")[-1]
+        no_ext = filename.split(".")[0]
+        path_no_ext = "/data/web_static/releases/{}/".format(no_ext)
+        symlink = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}".format(path_no_ext))
+        run("tar -xzf /tmp/{} -C {}".format(filename, path_no_ext))
+        run("rm /tmp/{}".format(filename))
+        run("mv {}web_static/* {}".format(path_no_ext, path_no_ext))
+        run("rm -rf {}web_static".format(path_no_ext))
+        run("rm -rf {}".format(symlink))
+        run("ln -s {} {}".format(path_no_ext, symlink))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
