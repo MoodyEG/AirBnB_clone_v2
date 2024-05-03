a11b65ff5c61cd8e2206807ad3d54a42021a54f5
MoodyEG <moodyeg2011@yahoo.com>
2024-05-03 12:44:18 -0700

    done fast

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from fabric.api import local
+from time import strftime
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
+    timenow = strftime("%Y%M%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        filename = "versions/web_static_{}.tgz".format(timenow)
+        local("tar -cvzf {} web_static/".format(filename))
+        return filename
+    except:
+        return None
