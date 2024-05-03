4f84c348460a703689a242fcbf2db4c721aff8a6
MoodyEG <moodyeg2011@yahoo.com>
2024-05-03 16:52:51 -0700

    done fast

---
+++ 100-clean_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Main
+Usage:
+fab -f 100-clean_web_static.py do_clean:number=2 -i
+my_ssh_private_key -u ubuntu > /dev/null 2>&1
+"""
+from fabric.api import local, env, put, run  # type: ignore
+
+
+def do_clean(number=0):
+    """ DELETE """
+    number = int(number)
+    if number == 0:
+        number = 2
+    else:
+        number += 1
+    local("cd versions ; ls -t | tail -n +{} | xargs rm -rf".format(number))
+    path = "/data/web_static/releases"
+    run("cd {} ; ls -t | tail -n +{} | xargs rm -rf".format(path, number))
