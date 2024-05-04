8f9909aa2521aec6eaeb3dfcbf508bf98ce99437
MoodyEG <moodyeg2011@yahoo.com>
2024-05-03 19:06:20 -0700

    done fast

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,35 +1,22 @@
 #!/usr/bin/python3
-"""This module defines the function do_clean
+""" Main
+Usage:
+fab -f 100-clean_web_static.py do_clean:number=2 -i
+my_ssh_private_key -u ubuntu > /dev/null 2>&1
 """
-from fabric.api import *
+from fabric.api import local, env, put, run  # type: ignore
 
 
 env.hosts = ['52.23.178.135', '100.25.47.15']
-env.user = 'ubuntu'
 
 
 def do_clean(number=0):
-    """This function deletes out-of-date archives
-    """
-    output = local("ls -lt ./versions/", capture=True)
-    output = output.split('\n')
-    output.pop(0)
+    """ DELETE """
     number = int(number)
     if number == 0:
-        number = 1
-    i = number
-    while i <= (len(output) - 1):
-        filename = output[i].split(' ')[-1]
-        local("rm -rf ./versions/{}".format(filename))
-        i += 1
-
-    output = run("ls -lt /data/web_static/releases")
-    output = output.split('\n')
-    output.pop(0)
-    print(output)
-    i = int(number)
-    while i <= (len(output) - 1):
-        filename = output[i].split(' ')[-1]
-        sudo("rm -rf /data/web_static/releases/{}".format(filename))
-        i += 1
-
+        number = 2
+    else:
+        number += 1
+    local("cd versions ; ls -t | tail -n +{} | xargs rm -rf".format(number))
+    path = "/data/web_static/releases"
+    run("cd {} ; ls -t | tail -n +{} | xargs rm -rf".format(path, number))
