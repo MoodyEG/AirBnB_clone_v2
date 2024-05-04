242184c63a3ed5acb2535529dee8c1ff46f59cdf
MoodyEG <moodyeg2011@yahoo.com>
2024-05-03 20:22:17 -0700

    done fast

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -12,6 +12,11 @@ exec { 'install':
   sudo mkdir -p /data/web_static/releases/test/ ; sudo mkdir -p /data/web_static/shared/ ; \
   echo "simple content" | sudo tee /data/web_static/releases/test/index.html ; \
   sudo ln -sf /data/web_static/releases/test/ /data/web_static/current ; sudo chown -R ubuntu:ubuntu /data/ ; \
-  sudo sed -i "60i \tlocation \/hbnb_static\/ {\n\t\talias \/data\/web_static\/current\/;\n\t}\n" /etc/nginx/sites-available/default ; \
+  echo "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\
+  \n\troot /var/www/html;\n\tindex home.html index.html index.htm index.nginx-debian.html;\
+  \n\tserver_name _;\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n\tlocation \
+  /redirect_me {\n\t\treturn 301 https://image.pngaaa.com/935/3311935-middle.png;\n\t}\n\tlocation \
+  / {\n\t\ttry_files \$uri \$uri/ =404;\n\t}
+  }" | sudo tee /etc/nginx/sites-available/default ; \
   sudo service nginx restart ; exit 0',
 }
